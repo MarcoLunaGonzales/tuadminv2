f5b2b401e7df092d81fc2e543c2af23e838026f6
Marco Luna <lunagonzalesmarco@gmail.com>
2023-10-09 16:10:03 -0400

    cambios

--- procesarCostosAlmacen.php
+++ procesarCostosAlmacen.php
@@ -11,7 +11,7 @@ set_time_limit(0);
 $codAlmacen=$_GET["cod_almacen"];
 
 $sqlProductos="SELECT distinct(sd.cod_material) from salida_almacenes s, salida_detalle_almacenes sd
-where s.cod_salida_almacenes=sd.cod_salida_almacen and s.cod_almacen='$codAlmacen' and s.fecha BETWEEN '2023-09-01' and '2023-09-30' and s.salida_anulada=0";
+where s.cod_salida_almacenes=sd.cod_salida_almacen and s.cod_almacen='$codAlmacen' and s.fecha BETWEEN '2023-01-01' and '2023-12-31' and s.salida_anulada=0";
 $respProductos=mysqli_query($enlaceCon,$sqlProductos);
 
 while($datProductos=mysqli_fetch_array($respProductos)){
