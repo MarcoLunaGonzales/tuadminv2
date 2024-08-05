508506f26beedcc98ae328a5d6eed47e01796dc7
Ronald <45970198+roalxDcar@users.noreply.github.com>
2024-08-05 10:30:40 -0400

    .

--- indexSupervision.php
+++ indexSupervision.php
@@ -165,6 +165,7 @@ $serverSIAT=obtenerValorConfiguracion(7);
 			</li-->
 			<!-- Versión Nueva -->
 			<li><a href="registrar_salidaventas.php" target='_blank'>Vender / Facturar</a></li>
+			<li><a href="navegadorCotizaciones.php" target='_blank'>Cotizaciones</a></li>			
 			<li><a href="listadoProductosStock.php" target='_blank'>Stock Actual **</a></li>
 			<li><a href="rptOpArqueoDiario.php?variableAdmin=1" target="contenedorPrincipal" >Arqueo de Caja</a></li>
 			
